37fce5bafe8b4590145719e681fc32e1b2733367
Luu Huu Hoan <xhoaluu@Luus-MacBook-Pro.local>
2025-04-01 14:07:37 +0700

    move to ws

--- app.js
+++ app.js
@@ -1,46 +1,18 @@
 const assert = require('assert');
-assert.ok(process.env.JAMBONZ_ACCOUNT_SID, 'You must define the JAMBONZ_ACCOUNT_SID env variable');
-assert.ok(process.env.JAMBONZ_API_KEY, 'You must define the JAMBONZ_API_KEY env variable');
-assert.ok(process.env.JAMBONZ_REST_API_BASE_URL, 'You must define the JAMBONZ_REST_API_BASE_URL env variable');
 assert.ok(process.env.VERSA_API_KEY, 'You must define the VERSA_API_KEY env variable');
 assert.ok(process.env.VERSA_BASE_URL, 'You must define the VERSA_BASE_URL env variable');
 assert.ok(process.env.APP_TRUNK_NAME, 'You must define the APP_TRUNK_NAME env variable');
 
-const express = require('express');
-const app = express();
-const {WebhookResponse} = require('@jambonz/node-client');
-const basicAuth = require('express-basic-auth');
-const opts = Object.assign({
-  timestamp: () => `, "time": "${new Date().toISOString()}"`,
-  level: process.env.LOGLEVEL || 'info'
-});
-const logger = require('pino')(opts);
 const port = process.env.HTTP_PORT || 3000;
-const routes = require('./lib/routes');
-app.locals = {
-  ...app.locals,
-  logger,
-  client: require('@jambonz/node-client')(process.env.JAMBONZ_ACCOUNT_SID, process.env.JAMBONZ_API_KEY, {
-    baseUrl: process.env.JAMBONZ_REST_API_BASE_URL
-  })
-};
+const {createServer} = require('http');
+const {createEndpoint} = require('@jambonz/node-client-ws');
+const server = createServer();
+const makeService = createEndpoint({server});
+const logger = require('pino')({level: process.env.LOGLEVEL || 'debug'});
 
-if (process.env.HTTP_USERNAME && process.env.HTTP_PASSWORD) {
-  const users = {};
-  users[process.env.HTTP_USERNAME] = process.env.HTTP_PASSWORD;
-  app.use(basicAuth({users}));
-}
-app.use(express.urlencoded({ extended: true }));
-app.use(express.json());
-if (process.env.WEBHOOK_SECRET) {
-  app.use(WebhookResponse.verifyJambonzSignature(process.env.WEBHOOK_SECRET));
-}
-app.use('/', routes);
-app.use((err, req, res, next) => {
-  logger.error(err, 'burped error');
-  res.status(err.status || 500).json({msg: err.message});
-});
+require('./lib/routes')({logger, makeService});
 
-const server = app.listen(port, () => {
-  logger.info(`Example jambonz app listening at http://localhost:${port}`);
+
+server.listen(port, () => {
+  logger.info(`jambonz websocket server listening at http://localhost:${port}`);
 });
